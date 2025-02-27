b65e7a504aa16e9943c404d050eb64f52ec422ed
glopez94 <glopez@istmocenter.com>
2025-02-27 11:44:07 -0600

    Move API endpoints to /api namespace

--- main.go
+++ main.go
@@ -1,11 +1,3 @@
-package main
-
-import (
-	"net/http"
-	"strconv"
-	"sync/atomic"
-)
-
 // import (
 // 	"net/http"
 // )
@@ -48,38 +40,29 @@ import (
 // 	server.ListenAndServe()
 // }
 
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
+//	type Handler interface {
+//		ServeHTTP(ResponseWriter, *Request)
+//	}
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"sync/atomic"
+)
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
+	// if r.Method != http.MethodGet {
+	// 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	// 	return
+	// }
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
-func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	hits := cfg.fileserverHits.Load()
-	w.Write([]byte("Hits: " + strconv.Itoa(int(hits))))
-}
-
-func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	cfg.fileserverHits.Store(0)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Write([]byte("Hits reset to 0"))
+type apiConfig struct {
+	fileserverHits atomic.Int32
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -89,13 +72,25 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte("Hits: " + strconv.Itoa(int(hits))))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("Hits reset to 0"))
+}
+
 func main() {
 	apiCfg := &apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", healthzHandler)
-	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
-	mux.HandleFunc("/reset", apiCfg.resetHandler)
+	mux.HandleFunc("GET /api/healthz", healthzHandler)
+	mux.HandleFunc("GET /api/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /api/reset", apiCfg.resetHandler)
 
 	fileServer := http.FileServer(http.Dir("."))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
